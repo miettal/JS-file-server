32834516756738c8732a86c9c5a4459f1687a881
miettal <iharahiromasa@gmail.com>
2013-11-06 02:25:55 +0900

    fix

--- main.js
+++ main.js
@@ -1,81 +1,123 @@
 var dir = "/";
 var files = [];
-var sort_by = "filename"
+var directories = [];
+var sort_by = "filename";
+var sort_inverse = false;
 location.hash = '#/';
 
+function unixtime2str(unixtime)
+{
+  var time = moment.unix(unixtime);
+  var str = time.format("YYYY/MM/DD HH:mm:ss");
+  
+  if(moment().startOf('day') < time){
+    str += ' Today';
+  }else if(moment().startOf('day').subtract('day') < time){
+    str += ' Yesterday';
+  }
+  
+  return str;
+}
+
+function filesize2str(filesize)
+{
+  var str = filesize;
+
+  if(filesize >= 1024*1024*1024){
+    str = filesize/1024/1024/1024+'G';
+  }else if(filesize >= 1024*1024){
+    str = filesize/1024/1024/1024+'M';
+  }else if(filesize >= 1024){
+    str = filesize/1024/1024/1024+'k';
+  }
+  
+  return str;
+}
+
 function rewrite_table(){
-  
   $("#panel_heading").text(dir);
-
-  $("#table tr:not(#head)").remove();
+  $("#table tbody tr").remove();
   
-  files.sort(
-    function(a,b){
-      if(a[sort_by] < b[sort_by]) return -1;
-      if(a[sort_by] > b[sort_by]) return 1;
-      return 0;
-    }
-  );
+  if(!sort_inverse){
+    files.sort(
+      function(a,b){
+        if(a[sort_by] < b[sort_by]) return -1;
+        if(a[sort_by] > b[sort_by]) return 1;
+        return 0;
+      }
+    );
+  }else{
+    files.sort(
+      function(a,b){
+        if(a[sort_by] < b[sort_by]) return 1;
+        if(a[sort_by] > b[sort_by]) return -1;
+        return 0;
+      }
+    );
+  }
 
-  $.each(files, function(index, file){
+  $.each(directories, function(index, file){
     if(file.filename == '.'){
       return true;
     }
-
     if(file.filename == '..' && dir === '/'){
+
       return true;
     }
 
-    var tr = $("<tr>").css("cursor","pointer");
+    var hash;
     if(file.filename == '..'){
-      tr.click(function(){
-        if(dir.split('/').slice(-1) !== ''){
-          parent_dir = dir.split('/').slice(0, -2).join('/');
-        }else{
-          parent_dir = dir.split('/').slice(0, -3).join('/');
-        }
-        location.hash = "#"+parent_dir+'/';
-      });
-      tr.append($("<td>").append('<span class="glyphicon glyphicon-arrow-up"></span>'));
-      tr.append($("<td>").text("parent directory"));
-      tr.append($("<td>").text("-"));
-      tr.append($("<td>").text(moment.unix(file.last_modified)));
-      $("#table").append(tr);
-      return true;
-    }
-
-    tr.click(function(){
-      if(file.type === "directory"){
-        location.hash += file.filename+'/';
+      if(dir.split('/').slice(-1) !== ''){
+        parent_dir = dir.split('/').slice(0, -2).join('/');
+      }else{
+        parent_dir = dir.split('/').slice(0, -3).join('/');
       }
-    });
+      hash = "#"+parent_dir+'/';
+    }else{
+      hash = location.hash + file.filename+'/';
+    }
 
-    if(file.type === "file"){
-      tr.append($("<td>").append('<span class="glyphicon glyphicon-file"></span>'));
-    }else{
-      tr.append($("<td>").append('<span class="glyphicon glyphicon-folder-close"></span>'));
-    }
-    tr.append($("<td>").text(file.filename));
-    if(file.type === "file"){
-      tr.append($("<td>").text(file.filesize));
-    }else{
-      tr.append($("<td>").text("-"));
-    }
-    tr.append($("<td>").text(moment.unix(file.last_modified)));
+    var tr = $("<tr>");
+    tr.append($("<td>").append('<span class="glyphicon glyphicon-folder-close"></span>'));
+    var a = $('<a>').attr('href', hash).text(file.filename);
+    tr.append($("<td>").append(a));
+    tr.append($("<td>").text("-"));
+    tr.append($("<td>").text(unixtime2str(file.last_modified)));
+
+    $("#table").append(tr);
+  });
+  
+  $.each(files, function(index, file){
+    var tr = $("<tr>");
+    tr.append($("<td>").append('<span class="glyphicon glyphicon-file"></span>'));
+    var a = $('<a>').attr('href', 'files'+dir+file.filename).text(file.filename).attr('download', file.filename);
+    tr.append($("<td>").append(a));
+    tr.append($("<td>").text(filesize2str(file.filesize)));
+    tr.append($("<td>").text(unixtime2str(file.last_modified)));
+
     $("#table").append(tr);
   });
 }
 
 $(function(){
 $("#filename").click(function(){
+  if(sort_by == "filename"){
+    sort_inverse = !sort_inverse;
+  }
   sort_by = "filename";
   rewrite_table();
 });
 $("#filesize").click(function(){
+  if(sort_by == "filesize"){
+    sort_inverse = !sort_inverse;
+  }
   sort_by = "filesize";
   rewrite_table();
 });
 $("#last_modified").click(function(){
+  if(sort_by == "last_modified"){
+    sort_inverse = !sort_inverse;
+  }
   sort_by = "last_modified";
   rewrite_table();
 });
@@ -88,7 +130,12 @@ $(window).hashchange(function() {
       dir:dir,
     },
     function(response){
-      files = response.files;
+      files = _.filter(response.files, function(file){
+        return file.type === "file";
+      });
+      directories = _.filter(response.files, function(file){
+        return file.type === "directory";
+      });
       rewrite_table();
     }
   )
